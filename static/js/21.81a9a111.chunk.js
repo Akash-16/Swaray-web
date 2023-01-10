6cd2e1267639a33a3d1c5ee72563975cea18d2ea
AkashP-calibraint <akashpalani@calibraint.com>
2023-01-10 15:26:51 +0530

    Updates

---
+++ static/js/21.81a9a111.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpswl-webapp"]=this["webpackJsonpswl-webapp"]||[]).push([[21],{405:function(p,s){}}]);
+//# sourceMappingURL=21.81a9a111.chunk.js.map
--- static/js/21.92c5da2d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpswl-webapp"]=this["webpackJsonpswl-webapp"]||[]).push([[21],{402:function(p,s){}}]);
-//# sourceMappingURL=21.92c5da2d.chunk.js.map
